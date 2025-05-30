cc0ce25cc99d1ab539164f2288d212c8c87a1f44
hydror <remy.roche75@gmail.com>
2025-05-30 13:32:44 +0200

    Delete website directory

--- website/admin/js/admin_config.js
+++
@@ -1,7 +0,0 @@
-// website/admin/js/admin_config.js
-
-// Define the base URL for your backend API
-const API_BASE_URL = 'http://localhost:5001'; // Base URL for all backend API calls
-                                              // Ensure your backend server is running on this address and port.
-// If you later decide to split your auth server or other admin APIs to different URLs,
-// you can re-introduce specific constants like ADMIN_AUTH_BASE_URL.
