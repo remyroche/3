5351a8b3a55c735f1c7e72a1b947a672c0e9c122
hydror <remy.roche75@gmail.com>
2025-06-05 17:28:23 +0200

    Create tailwind.config.js

---
+++ website/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js (example - this file doesn't exist in the current project structure)
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Raleway', 'sans-serif'],
+        serif: ['Baskervville', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
