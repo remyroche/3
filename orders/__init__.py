5b90028e225aa747f3495afdcd76de89ef97cc4a
hydror <remy.roche75@gmail.com>
2025-06-06 10:09:23 +0200

    Delete orders/__init__.py

--- orders/__init__.py
+++
@@ -1,8 +0,0 @@
-# backend/orders/__init__.py
-from flask import Blueprint
-
-orders_bp = Blueprint('orders_bp', __name__, url_prefix='/api/orders')
-
-# Import the refactored route modules to register their routes with the blueprint
-from . import public_routes
-from . import b2c_routes
