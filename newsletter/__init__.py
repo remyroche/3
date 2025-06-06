3e79b905efe934892771d06c4884bc551bdf7653
hydror <remy.roche75@gmail.com>
2025-06-06 10:18:19 +0200

    Update __init__.py

--- newsletter/__init__.py
+++ newsletter/__init__.py
@@ -1,5 +1,8 @@
-# backend/newsletter/__init__.py
+# newsletter/__init__.py
 from flask import Blueprint
-newsletter_bp = Blueprint('newsletter_bp', __name__, url_prefix='/api') # Prefix /api
-from . import routes
 
+newsletter_bp = Blueprint('newsletter', __name__, url_prefix='/api/newsletter')
+
+# Import the new B2B and B2C route modules
+from . import b2c_routes
+from . import b2b_routes
